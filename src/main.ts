a78190085ad8a02c63794664b8068aa7cb07c8ba
Kien <dao.cb@mz.co.kr>
2025-09-11 11:08:42 +0700

    chore: add primevue, zod, vue toastification

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,31 @@
 import '@/assets/css/style.css'
-
+import 'vue-toastification/dist/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura'
+import Toast, { POSITION } from 'vue-toastification'
+import clickOutsideDirective from '@/directives/click-outside'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {},
+    },
+});
+app.directive('click-outside', clickOutsideDirective)
+const options = {
+    position: POSITION.BOTTOM_RIGHT,
+    hideProgressBar: true,
+    closeButton: false,
+    closeOnClick: false,
+}
+app.use(Toast, options)
 
 app.mount('#root')
