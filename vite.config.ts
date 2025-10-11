f745450a661cb82e409f84f3724c404a8446c56b
Cien267 <kiennguyenduc267@gmail.com>
2025-10-12 06:09:59 +0700

    chore: update vite build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: '.',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
@@ -18,7 +19,6 @@ export default defineConfig({
   define: {
     'process.env': {},
   },
-  root: '.',
   build: {
     outDir: 'dist',
     rollupOptions: {},
